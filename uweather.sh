1692a40200252fb18fd7d37aa48ccbbead6a60a9
karlwithak <nhrynuik@gmail.com>
2013-04-24 13:00:11 -0300

    Create uweather.sh

---
+++ uweather.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lynx -source http://weather.uwaterloo.ca/waterloo_weather_station_data.xml | grep -e 'temperature' -e 'observation_time' -e 'wind_speed_kph' -e 'precipitation_1hr_mm' | sed -e 's/<\/.*>//' -e 's/>/:  /' -e 's/<//'
